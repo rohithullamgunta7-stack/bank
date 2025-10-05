d82cdc6d7b73ff8ee1166c9bcae11ae6be589116
u-jaswanth <jassullamgunta06@gmail.com>
2025-10-05 22:21:21 +0530

    Updated UserApp.js with improved login/signup UI and error handling

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,24 @@
 
 // export default API_BASE_URL;
 // src/config.js
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://foodhub-support-system-2.onrender.com';
-export const WS_BASE_URL = process.env.REACT_APP_WS_URL || 'ws://foodhub-support-system-2.onrender.com';
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_URL || 'https://foodhub-support-system-2.onrender.com';
+
+// export const WS_BASE_URL =
+//   process.env.REACT_APP_WS_URL || 'wss://foodhub-support-system-2.onrender.com';
+
+// export default API_BASE_URL;
+
+
+const isLocal = window.location.hostname === "localhost";
+
+const API_BASE_URL = isLocal
+  ? "http://127.0.0.1:8000"
+  : process.env.REACT_APP_API_URL || "https://foodhub-support-system-2.onrender.com";
+
+export const WS_BASE_URL = isLocal
+  ? "ws://127.0.0.1:8000"
+  : process.env.REACT_APP_WS_URL || "wss://foodhub-support-system-2.onrender.com";
 
 export default API_BASE_URL;
+
